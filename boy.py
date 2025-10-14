dbc797d02e538ace64d60ab03e3a1fc29bdc0573
jswlisa <jswlisa@tukorea.ac.kr>
2025-10-14 16:47:43 +0900

    auto run시 나타나는 캐릭터 크기 조정

--- boy.py
+++ boy.py
@@ -43,9 +43,9 @@ class AutoRun:
 
     def draw(self):
         if self.boy.face_dir == 1: # right
-            self.boy.image.clip_draw(self.boy.frame * 100, 100, 100, 100, self.boy.x , self.boy.y ,100,100)
+            self.boy.image.clip_draw(self.boy.frame * 100, 100, 100, 100, self.boy.x , self.boy.y ,250,250)
         else: # face_dir == -1: # left
-            self.boy.image.clip_draw(self.boy.frame * 100, 0, 100, 100, self.boy.x, self.boy.y,100,100)
+            self.boy.image.clip_draw(self.boy.frame * 100, 0, 100, 100, self.boy.x, self.boy.y,250,250)
 
 
 class Run:
